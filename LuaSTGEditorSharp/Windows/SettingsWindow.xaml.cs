f7dc36053a04863a402818f559bae7ef123397e5
Tania <hakussura@protonmail.com>
2024-07-10 23:14:43 +0200

    Remove Extended.Wpf, made a MaskedNumericInput to replace it.

--- LuaSTGEditorSharp/Windows/SettingsWindow.xaml.cs
+++ LuaSTGEditorSharp/Windows/SettingsWindow.xaml.cs
@@ -530,6 +530,32 @@ namespace LuaSTGEditorSharp.Windows
         }
 
         #endregion
+        #region Integer box integration
+
+        private void MaskNumericInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !TextIsNumeric(e.Text);
+        }
+
+        private void MaskNumericPaste(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string input = (string)e.DataObject.GetData(typeof(string));
+                if (!TextIsNumeric(input)) e.CancelCommand();
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private bool TextIsNumeric(string input)
+        {
+            return input.All(c => Char.IsDigit(c) || Char.IsControl(c));
+        }
+
+        #endregion
 
         public string TargetVersion
         {
